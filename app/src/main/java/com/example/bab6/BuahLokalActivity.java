3418eb4dfdb7633136146e84a67b37994f049f39
EmmilySisilia <emmily5446@gmail.com>
2022-11-22 14:17:13 +0700

    first commit

---
+++ app/src/main/java/com/example/bab6/BuahLokalActivity.java
@@ -0,0 +1,14 @@
+package com.example.bab6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BuahLokalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buah_lokal);
+    }
+}
